d1b801ffb809fc7d49e637117b9d744a202df6d4
洋 汪 <skywang12321@outlook.com>
2018-05-30 15:59:55 +0800

    初始化读数据

--- SetKCovering/SetKCovering.cpp
+++ SetKCovering/SetKCovering.cpp
@@ -2,10 +2,37 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <fstream>
 
-
+using namespace std;
+int n;
+int m;
+int *cost;
+int **AMatrix;
+void ReadFIle(char *filename)
+{
+	ifstream inf(filename);
+	inf >> m >> n;
+	cost = new int[n];
+	AMatrix = new int *[m];
+	for (int i = 0; i < n; ++) {
+		inf >> inf[i];
+	}
+	int RowSum;
+	int Columni;
+	for (int i = 0; i < m; i++) {
+		inf >> RowSum;
+		AMatrix[i] = new int[n](0);
+		for (int j = 0; j < RowSum; j++) {
+			inf >> Columni;
+			AMatrix[i][Columni] = 1;
+		}
+	}
+}
 int main()
 {
+
     return 0;
 }
 
